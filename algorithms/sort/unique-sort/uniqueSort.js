3e03ed0416db5d79767f59e367e00f9a8df493fc
Brad <brad@viziworx.com>
2020-03-30 13:28:39 -0400

    adds a simple sorting algorithm that should not return any duplicate values in the sorted array.

---
+++ algorithms/sort/unique-sort/uniqueSort.js
@@ -0,0 +1,21 @@
+//
+// Optimization with Caching
+// Unique Sort
+//
+
+// A simple sorting algorith that should not return any duplicate values in the sorted array.
+
+const uniqueSort = function(arr) {
+  const breadcrumbs = {};
+  const result = [arr[0]];
+
+  for (let i = 1; i < arr.length; i++) { // start loop at 1 as element 0 can never be a duplicate
+    if (!breadcrumbs[arr[i]]) {
+        result.push(arr[i]);
+        breadcrumbs[arr[i]] = true;
+    }
+  }
+  return result.sort((a, b) => a - b);
+}
+
+uniqueSort([6,4,4,7,1,1,1,10,10]) // [ 1, 4, 6, 7, 10 ]
